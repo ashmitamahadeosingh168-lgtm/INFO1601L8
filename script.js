fa2a167de76d384278eb67915e70ba1dd5837f68
ashmitamahadeosingh168-lgtm <ashmitamahadeosingh168@gmail.com>
2026-04-17 23:23:09 -0400

    added functions

--- script.js
+++ script.js
@@ -14,35 +14,32 @@ async function navigate(title, url){
   if(url === null){
     content.innerHTML = "";
   }else{
-    let response = await fetch(url);//fetch the page eg battery.html
+    let response = await fetch(url);
     content.innerHTML = await response.text();
     executeScripts();
   }
 }
 
-//event handler
 function handleClick(event){
   event.preventDefault();
   event.stopPropagation();
-  let a = event.target;//get the anchor tag element
-  let text = a.text;//get text content of the anchor element
-  let url = a.href;//get the url of the anchor element
-  history.pushState({title:text, url: url}, null, a.href);//pass the url and text to the history
-  navigate(a.text, a.href);//then navigate to page
+  let a = event.target;
+  let text = a.text;
+  let url = a.href;
+  history.pushState({title:text, url: url}, null, a.href);
+  navigate(a.text, a.href);
 }
 
 const menu = document.querySelector('#menu');
 menu.addEventListener('click', handleClick, false);
 
 function handleBack(event){
-  //if no links were clicked pushState() is never called
-  //as pushState() is never called there will be no data in event.state
+  
   if(event.state == null){
     navigate('Web APIs', null);
   }else{
-    //links were clicked before so we can get the text and url passed from handleClick()
     navigate(event.state.title, event.state.url);  
   }
 }
 
-window.addEventListener('popstate', handleBack);//attach event handler to back navigation
+window.addEventListener('popstate', handleBack);
